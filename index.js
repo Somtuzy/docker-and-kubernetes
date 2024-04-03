97ef54f13410ec6746b0ae25277cf156d43cc200
somtuzy <somtuzyonyeka@gmail.com>
2024-04-04 00:00:57 +0100

    Second commit

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express() 
 
+app.get('/', (req, res) => {
+    return res.send('Yessssssssss')
+})
+
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
